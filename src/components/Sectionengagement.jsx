422a906b0e16c94dcf8e9c34bcff0e39140d7383
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-05-05 17:56:42 +0530

    add

--- src/components/Sectionengagement.jsx
+++ src/components/Sectionengagement.jsx
@@ -1,17 +1,73 @@
 import styles from "../style";
 
-const Sectionengagement = () => (
+const CTA = () => (
   <section
-    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col `}
   >
-    <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Let’s try our service now!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
-      </p>
+    <div className="grid grid-cols-3 gap-8">
+      <div
+        className={`${styles.padding} sm:flex-row flex-col  w-[350px] h-[550px] font-poppins font-medium text-[18px] text-primary bg-orange-gradient rounded-[10px] outline-none`}
+      >
+        <h1
+          className={`${styles.flexStart} font-poppins font-bold ss:text-[16px] text-[20px] text-white ss:leading-[100.8px] leading-[75px] w-full`}
+        >
+          PRE-EVENT ENGAGEMENT
+        </h1>
+        <p className={`${styles.flexStart} px-[10px] font-poppins font-medium ss:text-[12px] text-[20px] text-white w-full`}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+          provident molestias officia debitis facilis voluptas excepturi libero
+          praesentium minus tempora temporibus saepe dignissimos cum, et ab
+          harum ut placeat a. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Esse provident molestias officia debitis facilis
+          voluptas excepturi libero praesentium minus tempora temporibus saepe
+          dignissimos cum, et ab harum ut placeat a. Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit.
+        </p>
+      </div>
+
+      <div
+        className={`${styles.padding} sm:flex-row flex-col  w-[350px] h-[550px] font-poppins font-medium text-[18px] text-primary bg-orange-gradient rounded-[10px] outline-none`}
+      >
+        <h1
+          className={`${styles.flexStart} font-poppins font-bold ss:text-[16px] text-[20px] text-white ss:leading-[100.8px] leading-[75px] w-full`}
+        >
+          PRE-EVENT ENGAGEMENT
+        </h1>
+        <p className={`${styles.flexStart} px-[10px] font-poppins font-medium ss:text-[12px] text-[20px] text-white w-full`}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+          provident molestias officia debitis facilis voluptas excepturi libero
+          praesentium minus tempora temporibus saepe dignissimos cum, et ab
+          harum ut placeat a. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Esse provident molestias officia debitis facilis
+          voluptas excepturi libero praesentium minus tempora temporibus saepe
+          dignissimos cum, et ab harum ut placeat a. Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit.
+        </p>
+      </div>
+
+      <div
+        className={`${styles.padding} sm:flex-row flex-col  w-[350px] h-[550px] font-poppins font-medium text-[18px] text-primary bg-orange-gradient rounded-[10px] outline-none`}
+      >
+        <h1
+          className={`${styles.flexStart} font-poppins font-bold ss:text-[16px] text-[20px] text-white ss:leading-[100.8px] leading-[75px] w-full`}
+        >
+          PRE-EVENT ENGAGEMENT
+        </h1>
+        <p className={`${styles.flexStart} px-[10px] font-poppins font-medium ss:text-[12px] text-[20px] text-white w-full`}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+          provident molestias officia debitis facilis voluptas excepturi libero
+          praesentium minus tempora temporibus saepe dignissimos cum, et ab
+          harum ut placeat a. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Esse provident molestias officia debitis facilis
+          voluptas excepturi libero praesentium minus tempora temporibus saepe
+          dignissimos cum, et ab harum ut placeat a. Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit.
+        </p>
+      </div>
     </div>
   </section>
 );
 
-export default Sectionengagement;
+export default CTA;
+
+// bg-black-gradient-2 rounded-[20px] box-shadow
