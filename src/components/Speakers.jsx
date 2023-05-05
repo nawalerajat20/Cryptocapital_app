f981ee5ba59102eb5dc0d0dea0819d1194f27b00
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-05-06 01:51:09 +0530

    add

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -8,17 +8,33 @@ const Speakers = () => (
   >
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full  bottom-40" />
 
-    <div className="w-full flex text-bold text-secondaryjustify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-      <h2 className={`${styles.heading2}`}>
-        2023 LINE UP <br className="sm:block hidden" /> 20+ SPEAKERS
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2
+        className={`${styles.heading2} font-poppins font-extrabold ss:text-[60px] text-[40px] text-dimBlack ss:leading-[90px] leading-[75px]`}
+      >
+        2023 LINE UP <br />{" "}
+        <span className="text-secondary"> 20+ SPEAKERS</span>
       </h2>
+
       <div className="w-full md:mt-0 mt-6">
-        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+        <p className={`${styles.paragraph} text-left max-w-[500px]`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+          ratione sapiente neque ab atque qui excepturi. Illum autem facilis
+          consequuntur alias quaerat sapiente dolorem, possimus eaque molestiae,
+          earum vero ab. Illum autem facilis consequuntur alias quaerat sapiente
+          dolorem, possimus eaque molestiae, earum vero ab.
         </p>
       </div>
+      
     </div>
+
+    <div className="flex flex-row items-center py-[6px] px-4 bg-black-gradient-2 rounded-[10px] mb-2">
+          <p className={`${styles.paragraph} `}>
+            <span className="text-white">
+              More Speakers to be announced soon!
+            </span>
+          </p>
+        </div>
 
     <div className="grid grid-cols-3 gap-8">
       <div
