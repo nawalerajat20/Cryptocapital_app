b4a9d264b610f92c64f3da1bac5be17b568559a4
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-12-27 09:10:02 +0530

    add

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,98 +3,25 @@ import styles from "../style";
 
 import {
   Speakers,
-  Ted,
-  Tedx,
-  Tedxdyp,
-  Deliverables,
-  Attend,
-  Chapter,
-  Insights,
-  Stats,
-  Theme,
-  Hero,
-  Transending,
-  Activities,
-  Packages,
-  Client,
-  Timeline,
-  // ExploreMore,
 } from "../components";
+import Work from "../components/Work";
+import AboutUs from "../components/AboutUs";
 
 const MainPage = () => {
   return (
     <>
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero />
-        </div>
+      <div className={`${styles.boxWidth}`}>
+        <AboutUs />
       </div>
-
-      <div className={`bg-dimRed ${styles.flexStart}`}>
-        <div className={`flex-1 ${styles.boxWidth}`}>
-          <Stats />
-        </div>
-      </div>
-
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
+    </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`flex ${styles.boxWidth}`}>
-          <Ted />
+          <Work />
         </div>
-      </div>
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
-        <div className={`flex ${styles.boxWidth}`}>
-          <Tedx />
-        </div>
-      </div>
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
-        <div className={`flex ${styles.boxWidth}`}>
-          <Tedxdyp />
-        </div>
-      </div>
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
-        <div className={`flex ${styles.boxWidth}`}>
-          <Chapter />
-        </div>
-      </div>
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
-        <Theme />
-      </div>
-      <div className={`bg-secondary ${styles.flexStart}`}>
-        <Transending />
       </div>
       <div className={`bg-grey ${styles.flexStart}`}>
         <Speakers />
-      </div>
-
-      <div className={`bg-dimBlue ${styles.flexStart}`}>
-        <Attend />
-      </div>
-      <div className={`bg-black ${styles.flexStart}`}>
-        <Insights />
-      </div>
-
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
-        <div className={`flex ${styles.boxWidth}`}>
-          <Timeline />
-        </div>
-      </div>
-
-      <div className={`bg-reds-gradient ${styles.flexStart}`}>
-        <div className={`flex ${styles.boxWidth}`}>
-          <Deliverables />
-        </div>
-      </div>
-
-      <div className={`bg-dimBlue ${styles.flexStart}`}>
-        <div className="flex sm:max-w-[1000px] w-full">
-          <Activities />
-        </div>
-      </div>
-      <div className={`bg-dimBlue ${styles.flexStart}`}>
-        <Packages />
-      </div>
-      <div className={`bg-red-gradient ${styles.flexStart}`}>
-        <Client />
       </div>
     </>
   );
