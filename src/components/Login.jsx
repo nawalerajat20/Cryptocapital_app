b846be49c0d78f4df2d70e3ba69cc065aa77a16e
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2024-04-03 13:02:08 +0530

    add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,51 +1,33 @@
-// import { Googlelogo } from "../assets";
-
-// const LoginPage = () => (
-//   <div className="flex items-center justify-center h-screen mt-[50px] bg-primary">
-//     <div className="bg-darkGrey rounded-[20px] px-10 py-12 text-white w-[400px]">
-//       <h2 className="text-2xl font-medium mb-8">Log In</h2>
-//       <div className="mb-6">
-//         <input
-//           type="text"
-//           id="username"
-//           placeholder="Username"
-//           className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
-//         />
-//       </div>
-//       <div className="mb-4">
-//         <input
-//           type="password"
-//           id="password"
-//           placeholder="Password"
-//           className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
-//         />
-//       </div>
-//       <button className="bg-dimBlue text-white px-2 py-2 ml-[5.2rem] rounded-md mb-2 w-40">
-//         Log In
-//       </button>
-//       <span className="text-sm text-gray-400 block uppercase text-center mb-2">
-//         or
-//       </span>
-//       <button className="text-white px-2 py-2 rounded-md w-60  left-[40px] border border-white relative">
-//         <span className="ml-[1rem]">Login with Google</span>
-//         <img
-//           src={Googlelogo}
-//           alt="Google Logo"
-//           className="w-9 h-8 absolute right-50 left-[10px] top-1/2 transform -translate-y-1/2"
-//         />
-//       </button>
-//       <p className="text-sm text-dimBlue text-center mt-4">Forgot Password?</p>
-//     </div>
-//   </div>
-// );
-
-// export default LoginPage;
-
-import { useState } from 'react';
+import { useState } from "react";
 import { Googlelogo } from "../assets";
+import { Signup } from "./api/cryptoapi";
 
 const LoginPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const handleSignup = async (e) => {
+    console.log("singup initiated");
+    const signupObject = {
+      name,
+      email,
+      username,
+      password,
+      passwordConfirm,
+    };
+    const response = await Signup(signupObject);
+    if (response.status == 201) {
+      console.log(response.data.token);
+      localStorage.setItem("token", response.data.token);
+    } else {
+      console.log(response);
+    }
+    console.log(response);
+  };
 
   const toggleLoginSignup = () => {
     setIsLogin(!isLogin);
@@ -54,13 +36,29 @@ const LoginPage = () => {
   return (
     <div className="flex items-center justify-center h-screen mt-[50px] bg-primary">
       <div className="bg-darkGrey rounded-[20px] px-10 py-12 text-white w-[400px]">
-        <h2 className="text-2xl font-semibold mb-8">{isLogin ? 'Login' : 'Sign Up'}</h2>
+        <h2 className="text-2xl font-semibold mb-8">
+          {isLogin ? "Login" : "Sign Up"}
+        </h2>
+        <div className="mb-6">
+          <input
+            type="text"
+            id="Name"
+            placeholder="Name"
+            className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
         <div className="mb-6">
           <input
             type="text"
             id="username"
             placeholder="Username"
             className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
           />
         </div>
         {!isLogin && (
@@ -70,6 +68,9 @@ const LoginPage = () => {
               id="email"
               placeholder="Email"
               className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
           </div>
         )}
@@ -79,6 +80,9 @@ const LoginPage = () => {
             id="password"
             placeholder="Password"
             className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </div>
         {!isLogin && (
@@ -88,11 +92,17 @@ const LoginPage = () => {
               id="confirmPassword"
               placeholder="Confirm Password"
               className="w-full px-3 py-2 rounded-md bg-darkGrey text-[15px] text-white border border-white"
+              onChange={(e) => {
+                setPasswordConfirm(e.target.value);
+              }}
             />
           </div>
         )}
-        <button className="bg-dimBlue text-white px-2 py-2 ml-[5.2rem] rounded-md mb-2 w-40">
-          {isLogin ? 'Log In' : 'Sign Up'}
+        <button
+          onClick={handleSignup}
+          className="bg-dimBlue text-white px-2 py-2 ml-[5.2rem] rounded-md mb-2 w-40"
+        >
+          {isLogin ? "Log In" : "Sign Up"}
         </button>
         <span className="text-sm text-gray-400 block uppercase text-center mb-2">
           or
@@ -101,9 +111,7 @@ const LoginPage = () => {
           className="text-white px-2 py-2 rounded-md w-60 left-[40px] border border-white relative"
           onClick={toggleLoginSignup}
         >
-          <span className="ml-[1rem]">
-            {isLogin ? 'Sign Up' : 'Log In'}
-          </span>
+          <span className="ml-[1rem]">{isLogin ? "Sign Up" : "Log In"}</span>
           <img
             src={Googlelogo}
             alt="Google Logo"
@@ -111,7 +119,7 @@ const LoginPage = () => {
           />
         </button>
         <p className="text-sm text-dimBlue text-center mt-4">
-          {isLogin ? 'Forgot Password?' : ''}
+          {isLogin ? "Forgot Password?" : ""}
         </p>
       </div>
     </div>
